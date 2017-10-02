bef0fcb3748d77375c269ac4cba470542744a18a
llafuente <llafuente@noboxout.com>
2017-10-02 23:58:19 +0200

    fix: use numbers instead of letter for tests

--- angular/src/app/Test/Test.component.ts
+++ angular/src/app/Test/Test.component.ts
@@ -15,7 +15,21 @@ import { LoggedUser } from "../LoggedUser.service";
   templateUrl: "./Test.component.html",
 })
 export class TestComponent extends BaseComponent {
-  answerIndexes = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"];
+  answerIndexes = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+    "13",
+  ];
   testId: string = null;
   test: ITest = null;
   remainingTime: number = null;
