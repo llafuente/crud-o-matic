e123006afa537e2aa0ef3215e03aaa533dabb547
llafuente <llafuente@noboxout.com>
2017-10-15 19:16:21 +0200

    feat: export to test to xml

--- angular/src/app/Statistics/StatisticsOne.component.ts
+++ angular/src/app/Statistics/StatisticsOne.component.ts
@@ -19,6 +19,9 @@ import {
 import { TestType } from "../../generated/src/models/ITest";
 import { UserType } from "../../generated/src/models/IUser";
 import { LoggedUser } from "../LoggedUser.service";
+import * as XLSX from "xlsx";
+
+import * as fileSaver from "file-saver";
 
 @Component({
   selector: "statistics-component",
@@ -46,8 +49,8 @@ export class StatisticsOneComponent extends BaseComponent {
   usersInTest: UserType[];
   testAnswers: number[];
   test: TestType = null;
-  tResults = { ok: 0, ko: 0 };
-  qResults = [];
+  tResults: { ok: number, ko: number } = { ok: 0, ko: 0 };
+  qResults: { ok: number, ko: number }[] = [];
 
   constructor(
     public injector: Injector,
@@ -147,25 +150,131 @@ export class StatisticsOneComponent extends BaseComponent {
           this.usersInTest.push(user);
           user.stats = null;
           (user as any).stat = stat; // this is the stat that matters
-          (user as any).sucesses = 0; // this is the stat that matters
-          (user as any).failures = 0; // this is the stat that matters
+          (user as any).ok = 0; // this is the stat that matters
+          (user as any).ko = 0; // this is the stat that matters
           stat.answers.forEach((answerIdx, idx) => {
             if (this.testAnswers[idx] == answerIdx) {
               ++this.tResults.ok;
               ++this.qResults[idx].ok;
-              ++(user as any).sucesses;
+              ++(user as any).ok;
             } else {
               ++this.tResults.ko;
               ++this.qResults[idx].ko;
-              ++(user as any).failures;
+              ++(user as any).ko;
             }
           });
 
-          (user as any).result = (user as any).sucesses * 100;
+          (user as any).result = (user as any).ok * 100;
           (user as any).result /= this.testAnswers.length;
           (user as any).result = Math.floor((user as any).result);
         }
       }
     });
+  }
+
+  toExcel() {
+    const answersData: any[] = [
+      ["Pregunta", "Respuesta correcta"]
+    ];
+    const questionsData: any[] = [
+      ["Pregunta", "Aciertos", "Errores"]
+    ];
+    const usersData: any[] = [
+      ["Nombre y apellidos"]
+    ];
+
+    const testData: any[] = [
+      ["Examen", this.test.label],
+      ["Tags", this.test.tags],
+      ["Aciertos totales", this.tResults.ok],
+      ["Errores totales", this.tResults.ko],
+      [],
+      ["Exámen"],
+    ];
+    let i = 1;
+    for (let block of this.test.blocks) {
+      for (let question of block.questions) {
+        answersData.push([i, question.correcAnswerIndex]);
+        usersData[0].push(`Respuesta ${i}`);
+        ++i;
+      }
+    }
+    usersData[0].push(`Aciertos`);
+    usersData[0].push(`Errores`);
+    usersData[0].push(`%`);
+
+    i = 1;
+    for (let block of this.test.blocks) {
+      for (let question of block.questions) {
+        testData.push(["Pregunta", i, question.questionLabel]);
+        let j = 1;
+        for (let answer of question.answers) {
+          testData.push(["Respuesta", j, answer.answerLabel]);
+          ++j;
+        }
+        ++i;
+      }
+    }
+
+    i = 1;
+    for (let q of this.qResults) {
+      questionsData.push([i, q.ok, q.ko]);
+      ++i;
+    }
+
+    for (let user of this.usersInTest) {
+      usersData.push([user.name + " " + user.surname].concat((user as any).stat.answers).concat([
+        (user as any).ok,
+        (user as any).ko,
+        this.percentage((user as any).ok, (user as any).ko),
+      ]));
+    }
+
+
+    const workbook = {
+      SheetNames: ["Examen", "Respuestas", "Preguntas", "Usuarios"],
+      Sheets: {
+        Examen: XLSX.utils.aoa_to_sheet(testData),
+        Respuestas: XLSX.utils.aoa_to_sheet(answersData),
+        Preguntas: XLSX.utils.aoa_to_sheet(questionsData),
+        Usuarios: XLSX.utils.aoa_to_sheet(usersData),
+      }
+    };
+     
+    /* Add the sheet name to the list */
+    //workbook.SheetNames.push(ws_name);
+    /* Load the worksheet object */
+    //workbook.Sheets[ws_name] = ws;
+
+    //var wopts = { bookType:'xlsx', bookSST:false, type:'binary' };
+    var wopts = { bookType: 'xlml', bookSST: false, type: 'binary' };
+    var wbout = XLSX.write(workbook, wopts as any);
+
+    // console.log("wbout", wbout);
+    // this.downloadFile("export.xml", wbout);
+
+    function s2ab(s) {
+      var buf = new ArrayBuffer(s.length);
+      var view = new Uint8Array(buf);
+      for (var i = 0; i != s.length; ++i) view[i] = s.charCodeAt(i) & 0xFF;
+      return buf;
+    }
+
+    var blob = new Blob([s2ab(wbout)], {type: "text/xml;charset=utf-8"});
+
+
+    fileSaver.saveAs(blob, "export.xml", true);
+  }
+
+  downloadFile(filename: string, contents: string) {
+    var data, link;
+
+    link = document.createElement('a');
+    // 'data:text/csv;charset=utf-8,' + contents;
+    // link.setAttribute('href', data);
+    //link.setAttribute('href', 'data:text/xml;charset=utf-8,' + encodeURI(contents));
+    link.setAttribute('href', 'data:application/ocstream;charset=utf-8,' + encodeURI(contents));
+    link.setAttribute('download', filename);
+    link.click();
   }
 }
